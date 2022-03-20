241380e7ece06b9fb01335a64b21bdd53ab45106
ATC <83381296+zhaozirui-ML@users.noreply.github.com>
2022-03-20 17:58:30 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'a199bad26f2c43b4a4bb9a7eec9ca6eb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'ATC Workapce',
+  domain: 'https://wool-effect-528.notion.site/HI-ATC-a199bad26f2c43b4a4bb9a7eec9ca6eb',
+  author: 'ATC',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'zhaozirui3',
+  github: 'ATC',
+  instagram: 'atc12138',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
